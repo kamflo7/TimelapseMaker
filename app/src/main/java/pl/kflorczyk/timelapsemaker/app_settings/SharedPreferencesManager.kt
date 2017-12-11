a34d0f5ac81b9c952602778f2102f95783ff4071
Kamil <florczykkamil@gmail.com>
2017-12-11 17:21:17 +0100

    Remove guest password option in web access

--- app/src/main/java/pl/kflorczyk/timelapsemaker/app_settings/SharedPreferencesManager.kt
+++ app/src/main/java/pl/kflorczyk/timelapsemaker/app_settings/SharedPreferencesManager.kt
@@ -14,7 +14,6 @@ class SharedPreferencesManager(context:Context) {
 
     private val PACKAGE = "pl.kflorczyk.timelapsemaker"
     private val FILE_KEY = "$PACKAGE.settingsFile"
-    private val KEY_WEB_PASSWORD_GUEST = "$PACKAGE.settingsFile.webpasswordguest"
     private val KEY_WEB_PASSWORD_ADMIN = "$PACKAGE.settingsFile.webpasswordadmin"
     private val KEY_WEB_ENABLED = "$PACKAGE.settingsFile.webenabled"
     private val KEY_CAMERA_API = "$PACKAGE.settingsFile.cameraapi"
@@ -104,27 +103,6 @@ class SharedPreferencesManager(context:Context) {
 
     fun removeWebAdminPassword() {
         sharedPref.edit().remove(KEY_WEB_PASSWORD_ADMIN)
-    }
-
-    /**
-     * @return passwordExists:Boolean, the password if yes
-     */
-    fun getWebGuestPassword(): Pair<Boolean, String?> {
-        val password:String? = sharedPref.getString(KEY_WEB_PASSWORD_GUEST, null)
-        return Pair(password != null, password)
-    }
-
-    fun setWebGuestPassword(password: String): Boolean {
-        if(!PasswordValidator().validate(password)) {
-            return false;
-        }
-
-        sharedPref.edit().putString(KEY_WEB_PASSWORD_GUEST, password).apply()
-        return true
-    }
-
-    fun removeWebGuestPassword() {
-        sharedPref.edit().remove(KEY_WEB_PASSWORD_GUEST)
     }
 
     fun getWebEnabled(): Boolean = sharedPref.getBoolean(KEY_WEB_ENABLED, false)
